c18596ccc9fb1e0ad13bf2ff4a9ca13c8cebb9c8
PabloRR100 <pabloruizruiz8@gmail.com>
2017-06-12 17:45:20 -0300

    Eliminación de K malas y búsqueda de Pablo I

--- constantes.m
+++ constantes.m
@@ -10,27 +10,21 @@ function k = constantes(X, T, R, B, C)
 
     %k(1)  = 1.4282 * 10^10 * exp(-99064.12/(R*T));   % Datos Mejico
     k(1)  = 1.43 * 10^10 * exp(-111380/(R*T));         % Ajuste
-    %k(2)  = 2.8711 * 10^-11;  % Emilio
     k(2) = 0;
     k(3) = 4.92 * 10^5 * exp(-18195.54/(R*T));      % Mahabadi
 
     k(4)  = 4.92 * 10^5 * exp(-18195.54/(R*T));      % Mahabadi
-    %k(4)  = 4.92 * 10^7 * exp(-18153.74/(R*T));      % Matthew Justin --> SE DISPARA MUCHISIMO
 
     %k(5)  = 9.80 * 10^7 * exp(-2930.180/(R*T));      % Mahabadi
         ktd0 = 9.80 * 10^7 * exp(-2930.180/(R*T)); 
     k(5)  = ktd0 * exp( B*X + C*X^2);                 % Friis
     %k(5)   = ktd0 * (1 + B*X + C*X^2);                   % Pablo
-    %ktd  = (kp * M)^2 / (2*kd*ef*I3*(2/(1-X))^2);        % Pablo2
 
-    %k(6) = 0;                                          % Solo desproporción
     k(6) = 0.9 * k(5);                                 % Matthew Justin
-    %k(6) = (X/(1-X)) * k(5);                          % Pablo 2
-    %k(6) = 3.956*10^4 * exp(17096/(R*T));             % Bevington
-    %k(6) = k(7);                                       % Hacerlas iguales
+    %k(6) = (X/(1-X)) * k(5);                          % Pablo I
+    %k(6) = (X/(1-X^2)) * k(5);
+    %k(6) = (X/(1-X)^2) * k(5)
 
-    %k(7)  = 0.65;
-    %k(7) = k(4) * 9480 * exp(-58113/(R*T));
     %k(7)  = 2.41 * 10^9 * exp(-2930.180/(R*T));      % Matthew Justin
     k(7) = 2.012 * 10^9 * exp(-70454.84/(R*T));     % Ajuste del trabajo
 
